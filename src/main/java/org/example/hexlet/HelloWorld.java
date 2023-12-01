3fd5017c9e396a65848c4c9db99f286d21e31541
blonde2029 <mr_blonde@rambler.ru>
2023-12-01 09:51:16 +0700

    added param name

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -6,6 +6,19 @@ public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> config.plugins.enableDevLogging());
         app.get("/", ctx -> ctx.result("Hello, world!"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+
+        app.get("/hello", ctx -> {
+           var name = ctx.queryParam("name");
+           String hello = "";
+           if (name != null) {
+               hello = "Hello, " + name + "!";
+           } else {
+               hello = "Hello, World!";
+           }
+           ctx.result(hello);
+        });
         app.start(7070);
     }
 }
